ba9a01d8fd63828958e636c00d58565179f682bc
datle-dev <dle.dev@outlook.com>
2024-03-25 18:33:46 -0400

    Initial commit

---
+++ queue.js
@@ -0,0 +1,23 @@
+const Queue = () => {
+    let arr = [];
+
+    const enqueue = (value) => {
+        arr.push(value);
+    };
+
+    const dequeue = () => {
+        return arr.shift();
+    };
+
+    const print = () => {
+        console.log(arr);
+    };
+
+    return {
+        enqueue,
+        dequeue,
+        print,
+    }
+};
+
+export default Queue;
