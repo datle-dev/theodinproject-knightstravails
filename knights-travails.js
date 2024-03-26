675d629eb79c4f1f56e13a61856ccf8dc9200157
datle-dev <dle.dev@outlook.com>
2024-03-25 21:56:58 -0400

    Rework knightMoves function

--- knights-travails.js
+++ knights-travails.js
@@ -56,36 +56,49 @@ const Board = (function() {
 
     const knightMoves = (start, end) => {
         let queue = Queue();
-        let idCurrent = idFromCoords(start);
+        let idStart = idFromCoords(start);
         let idEnd = idFromCoords(end);
+        let idCurrent;
         let isEndReached = false;
+        let shortestPath = [];
 
-        queue.enqueue(idCurrent);
+        console.log(`knightMoves([${start}], [${end}]) --> `)
+
+        queue.enqueue(idStart);
 
         while (!isEndReached) {
+            idCurrent = queue.dequeue();
+            
             if (idCurrent == idEnd) {
                 isEndReached = true;
+                shortestPath.unshift(idEnd);
                 let currentParent = nodes[idEnd].getParent();
                 while (currentParent != null) {
+                    shortestPath.unshift(currentParent);
                     currentParent = nodes[currentParent].getParent();
                 }
                 break;
             }
 
-            if (nodes[idCurrent].isVisited()) {
-                idCurrent = queue.dequeue();
-            }
-
-            nodes[idCurrent].markVisited();
-
             adjacency[idCurrent].forEach((move) => {
-                if (nodes[move].isVisited()) {
+                if (nodes[move].isVisited() || nodes[move].isQueued()) {
                     return;
                 } else {
                     queue.enqueue(move);
+                    nodes[move].markQueued();
                     nodes[move].setParent(idCurrent);
                 }
             });
+
+            nodes[idCurrent].markVisited();
+        }
+
+        cleanUpNodes();
+        shortestPath = shortestPath.map((node) => [Number(node[0]), Number(node[1])]);
+        console.log(shortestPath);
+        return shortestPath;
+
+    };
 
     const cleanUpNodes = () => {
         for (const [id, node] of Object.entries(nodes)) {
