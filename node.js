4fccc38691c730aad32fdc88967aa653148bebc5
datle-dev <dle.dev@outlook.com>
2024-03-25 21:12:02 -0400

    Add get/set for queue status of a node

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
 export default function Node (id) {
     let parent = null;
     let visited = false;
+    let queued = false;
 
     const setParent = (idParent) => {
         parent = idParent;
@@ -18,11 +19,21 @@ export default function Node (id) {
         return visited;
     };
 
+    const markQueued = () => {
+        queued = true;
+    }
+
+    const isQueued = () => {
+        return queued;
+    }
+
     return {
         id,
         setParent,
         getParent,
         markVisited,
         isVisited,
+        markQueued,
+        isQueued,
     }
 };
